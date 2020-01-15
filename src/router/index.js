b9cac7efc7bd07218d06dfe128f6ca053090df46
Adam Richie-Halford <richford@uw.edu>
2020-01-15 12:57:58 -0800

    WIP: Change variable and filenames

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
-import Report from '@/components/Report';
-import Bucket from '@/components/Bucket';
+import GenReport from '@/components/GenReport';
+import ReportStandalone from '@/components/ReportStandalone';
 
 Vue.use(Router);
 
@@ -10,18 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'GenReport',
+      component: GenReport,
     },
     {
       path: '/report',
-      name: 'Report',
-      component: Report,
-    },
-    {
-      path: '/bucket/:bucket',
-      name: 'Bucket',
-      component: Bucket,
+      name: 'ReportStandalone',
+      component: ReportStandalone,
     },
   ],
 });
