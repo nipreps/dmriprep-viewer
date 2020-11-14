733105683ef85047477e618ddc9b1e5f7f2d92e1
Adam Richie-Halford <richiehalford@gmail.com>
2020-11-14 04:48:31 -0800

    STY: Okay, really fix prettier style in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/dmriprep-viewer/' : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/dmriprep-viewer/" : "/",
 };
